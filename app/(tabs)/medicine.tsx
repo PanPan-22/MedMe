4bae893a6fe8dddf7c7204ab78bb21d99c7e4b67
Chopin <chopin.won@gmail.com>
2026-02-12 17:19:33 +0700

    Commit

--- app/(tabs)/medicine.tsx
+++ app/(tabs)/medicine.tsx
@@ -1,7 +1,10 @@
 
 import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
 
 export default function MedicineScreen() {
     return(
+        <ThemedView>
         <ThemedText>Medicine Screen</ThemedText>
+        </ThemedView>
     )}
