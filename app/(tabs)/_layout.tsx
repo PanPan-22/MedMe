b3678fc839325fbc7cbc28a228b791a0f2a8ebc0
Chopin <chopin.won@gmail.com>
2026-02-12 16:20:07 +0700

    Commit Message

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,8 +26,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Exploration',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="medicine"
+        options={{
+          title: 'Medicine',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="pills.fill" color={color} />,
         }}
       />
     </Tabs>
