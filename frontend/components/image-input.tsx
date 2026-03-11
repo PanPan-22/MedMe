1d8c5d7577663c2c7a1450c29da3daa676dcc0d9
Chopin <chopin.won@gmail.com>
2026-03-11 22:15:56 +0700

    Scan image is here, but no parsing into json yet.

--- frontend/components/image-input.tsx
+++ frontend/components/image-input.tsx
@@ -1,10 +1,43 @@
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { Button, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ThemedText } from './themed-text';
+
+const API_KEY = "K88520222388957";
+const scanText = async (uri: string) => {
+    try {
+        const uriResponse = await fetch(uri);
+        const blob = await uriResponse.blob();
+        const base64 = await new Promise<string>((resolve) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const result = reader.result as string;
+                resolve(result.split(',')[1]);
+            }
+            reader.readAsDataURL(blob);
+        })
+
+        const formData = new FormData();
+        formData.append("base64Image",`data:image/jpeg;base64,${base64}`);
+        formData.append("language", "eng");
+
+        const response = await fetch(`https://api.ocr.space/parse/image`, {
+            method: 'POST',
+            headers: { apikey: API_KEY },
+            body: formData,
+        });
+        const data = await response.json();
+        const text = data?.ParsedResults?.[0]?.ParsedText || 'No text found';
+        return text;
+    } catch (error) {
+        console.error('scanText failed', error);
+        return 'Error scanning text from image. Please try again.';
+    }
+}
 
 const ImageInput = () => {
     const [image, setImage] = useState('');
+    const [scannedText, setScannedText] = useState('');
 
     const takePhoto = async () => {
         const perm = await ImagePicker.requestCameraPermissionsAsync();
@@ -33,17 +66,6 @@ const ImageInput = () => {
             setImage(result.assets[0].uri);
         }
     }
-    // const pickFromGallery = async () => {
-    //     const result = await
-    //         launchImageLibrary({
-    //             mediaType: 'photo',
-    //         },
-    //             (response) => {
-    //                 if (response.assets) {
-    //                     setImage(response.assets[0]);
-    //                 }
-    //             })
-    // };
 
     return (
         <View>
@@ -54,9 +76,45 @@ const ImageInput = () => {
             <TouchableOpacity onPress={handleImagePickerPress}>
                 <ThemedText type='subtitle'>Pick from Gallery</ThemedText>
             </TouchableOpacity>
-            {image && <Image source={{ uri: image }} style={{ width: 250, height: 250 }} resizeMode='contain' />}
+            {image !== null && (
+                <View style={styles.bigContainer}>
+                    <Image source={{ uri: image }} style={{ width: 250, height: 250 }} resizeMode='contain' />
+                    <View style={styles.buttonContainer}>
+                        <Button title="Scan!" onPress={async () => {
+                            const text = await scanText(image);
+                            setScannedText(text);
+                        }} />
+                    </View>
+                </View>
+            )}
+            {scannedText !== null && (
+                <ThemedText type='subtitle'>Scanned Text: {scannedText}</ThemedText>
+            )}
+            {/* //     {image && <Button title="Scan!" onPress={async () => {
+        //         const text = await scanText(image);
+        //         setScannedText(text);
+        //     }} />}
+        //     {scannedText && (
+        //         <ThemedText type='subtitle'>Scanned Text: {scannedText}</ThemedText>
+        //     )} */}
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        marginTop: 10,
+    },
+    bigContainer:{
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderColor: '#808080',
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 20,
+    }
+})
 
 export default ImageInput;
